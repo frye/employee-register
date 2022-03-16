1ab500d9105f54c728e9da6852b3bb86e772a15c
Samuli Jarvinen <frye@github.com>
2022-03-15 23:12:50 -0700

    Updating README.md and commenting out sample data input

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# employee-register
-Basic CLI employee register application.
+# Employee register
+
+## Description
+This node app is used to manage employee register. It allows you to manage departments, roles and employees (Some functionality is not yet fully implemented). The app is a console application.
+
+## License
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Table of contents
+- [Description](#description)
+- [License](#license)
+- [Installation](#installation)
+- [How to use](#how-to-use)
+- [More information](#more-information)
+
+## Installation
+- Clone this git repo
+- Execute ```npm i``` to install dependencies
+- Configure your database name and credentials in the .env file
+- Optional - Uncomment the sample data generation lines in app.js if you want that generated
+
+## How to use
+Launch the application using ```node index.js```
+
+
+
+
+
+
+## More information:
+- Through GitHub: [@frye](https://github.com/frye)
+- Via Email: psjcode@gmail.com
--- app.js
+++ app.js
@@ -404,58 +404,43 @@ const deleteEmployee = () => {
 
 
 // Remove force: true and bulk data creation once testing is done
-db.sync({ force: true })
+db.sync()
+	// .then(() => {
+	// 	console.log('Database connected, inserting sample data...');
+	// 	return Department.bulkCreate([
+	// 		{ name: 'Engineering' },
+	// 		{ name: 'HR' },
+	// 		{ name: 'Sales' },
+	// 		{ name: 'Marketing' },
+	// 		{ name: 'IT' },
+	// 		{ name: 'Accounting' },
+	// 	]);
+	// }
+	// ).then(() => {
+	// 	return Role.bulkCreate([
+	// 		{ title: 'CEO', salary: 100000, department_id: 1 },
+	// 		{ title: 'HR Manager', salary: 40000, department_id: 2 },
+	// 		{ title: 'HR Assistant', salary: 30000, department_id: 2 },
+	// 		{ title: 'Sales Manager', salary: 20000, department_id: 3 },
+	// 		{ title: 'Sales Assistant', salary: 10000, department_id: 3 },
+	// 		{ title: 'Marketing Manager', salary: 20000, department_id: 4 },
+	// 		{ title: 'Marketing Assistant', salary: 10000, department_id: 4 },
+	// 	]);
+	// })
+	// .then(() => {
+	// 	return Employee.bulkCreate([
+	// 		{ first_name: 'Mike', last_name: 'Manager', role_id: 1, manager_id: null },
+	// 		{ first_name: 'Helen', last_name: 'Manager', role_id: 2, manager_id: 1 },
+	// 		{ first_name: 'Hugh', last_name: 'Assistant', role_id: 3, manager_id: 1 },
+	// 		{ first_name: 'Sally', last_name: 'Manager', role_id: 4, manager_id: 2 },
+	// 		{ first_name: 'Sam', last_name: 'Assistant', role_id: 5, manager_id: 2 },
+	// 		{ first_name: 'Sue', last_name: 'Manager', role_id: 6, manager_id: 3 },
+	// 		{ first_name: 'Sara', last_name: 'Assistant', role_id: 7, manager_id: 3 },
+
+	// 	]);
+	// })
 	.then(() => {
-		console.log('Database connected, inserting sample data...');
-		return Department.bulkCreate([
-			{ name: 'Engineering' },
-			{ name: 'HR' },
-			{ name: 'Sales' },
-			{ name: 'Marketing' },
-			{ name: 'IT' },
-			{ name: 'Accounting' },
-		]);
-	}
-	).then(() => {
-		return Role.bulkCreate([
-			{ title: 'CEO', salary: 100000, department_id: 1 },
-			{ title: 'CTO', salary: 80000, department_id: 1 },
-			{ title: 'COO', salary: 70000, department_id: 1 },
-			{ title: 'CMO', salary: 60000, department_id: 1 },
-			{ title: 'CXO', salary: 50000, department_id: 1 },
-			{ title: 'HR Manager', salary: 40000, department_id: 2 },
-			{ title: 'HR Assistant', salary: 30000, department_id: 2 },
-			{ title: 'Sales Manager', salary: 20000, department_id: 3 },
-			{ title: 'Sales Assistant', salary: 10000, department_id: 3 },
-			{ title: 'Marketing Manager', salary: 20000, department_id: 4 },
-			{ title: 'Marketing Assistant', salary: 10000, department_id: 4 },
-			{ title: 'IT Manager', salary: 20000, department_id: 5 },
-			{ title: 'IT Assistant', salary: 10000, department_id: 5 },
-			{ title: 'Accounting Manager', salary: 20000, department_id: 6 },
-			{ title: 'Accounting Assistant', salary: 10000, department_id: 6 },
-		]);
-	})
-	.then(() => {
-		return Employee.bulkCreate([
-			{ first_name: 'John', last_name: 'Doe', role_id: 1, manager_id: null },
-			{ first_name: 'Jane', last_name: 'Doe', role_id: 2, manager_id: 1 },
-			{ first_name: 'Jack', last_name: 'Doe', role_id: 3, manager_id: 1 },
-			{ first_name: 'Jill', last_name: 'Doe', role_id: 4, manager_id: 1 },
-			{ first_name: 'Joe', last_name: 'Doe', role_id: 5, manager_id: 1 },
-			{ first_name: 'Jenny', last_name: 'Doe', role_id: 6, manager_id: 1 },
-			{ first_name: 'John', last_name: 'Doe', role_id: 7, manager_id: 2 },
-			{ first_name: 'Jane', last_name: 'Doe', role_id: 8, manager_id: 2 },
-			{ first_name: 'Jack', last_name: 'Doe', role_id: 9, manager_id: 2 },
-			{ first_name: 'Jill', last_name: 'Doe', role_id: 10, manager_id: 2 },
-			{ first_name: 'Joe', last_name: 'Doe', role_id: 11, manager_id: 2 },
-			{ first_name: 'Jenny', last_name: 'Doe', role_id: 12, manager_id: 2 },
-			{ first_name: 'John', last_name: 'Doe', role_id: 13, manager_id: 3 },
-			{ first_name: 'Jane', last_name: 'Doe', role_id: 14, manager_id: 3 },
-			{ first_name: 'Jack', last_name: 'Doe', role_id: 15, manager_id: 3 },
-		]);
-	})
-	.then(() => {
-		console.log('Sample data inserted\nLaunching app...');
+		// console.log('Sample data inserted\nLaunching app...');
 		displayMenu();
 	})
 	.catch(err => console.log(err));
